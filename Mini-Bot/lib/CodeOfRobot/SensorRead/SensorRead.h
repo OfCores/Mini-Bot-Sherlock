567f8170708072c8d50cc371d371940612ad3c54
Jones Braun <47891728+JonasBraun04@users.noreply.github.com>
2022-01-24 12:53:51 +0100

    new Librarys and completed SensorRead.h

--- Mini-Bot/lib/CodeOfRobot/SensorRead/SensorRead.h
+++ Mini-Bot/lib/CodeOfRobot/SensorRead/SensorRead.h
@@ -6,15 +6,13 @@
 class RemoteControl
 {
 private:
-    typedef struct  struct_message;
-
-    static void RemoteSetup();
-    static void RemoteLoop();
+    int[] getValues(Accuracy = Accuracy.Medium); //returns three precise values of the three sensors; Accuracy influences res of samples
+    int getRAWValueOf(BWSensor); //returns RAW value of a spez sensor
 
 public:
-    static void setup();
-    static void loop();
-    
+    static void optoSetup();
+    boolean isSensorOnLine(BWSensor); //returns value of selected Sensor (Sensor names saved in Enum) if on line return true if! return false 
+    boolean calibrate(Accuracy = Accuracy.Low); //Put Sherlock on a track and call function. Sherlock will auto kalibrate itself through turning left or right; return true if cali succesful; Accuracy intervens duration of calibration
 
 };
 
--- Mini-Bot/lib/CodeOfRobot/Util/Util.h
+++ Mini-Bot/lib/CodeOfRobot/Util/Util.h
@@ -1,1 +1,3 @@
 #include <Arduino.h>
+
+enum Accuracy {VeryLow,Low,Medium,High,VeryHigh}
