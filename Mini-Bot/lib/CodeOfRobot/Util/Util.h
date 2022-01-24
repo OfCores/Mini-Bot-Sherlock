84389ebb1e7417ab3f8363db50aebf4e1df71fcb
Jones Braun <47891728+JonasBraun04@users.noreply.github.com>
2022-01-24 18:47:37 +0100

    added Enum Accuracy again

--- Mini-Bot/lib/CodeOfRobot/Util/Util.h
+++ Mini-Bot/lib/CodeOfRobot/Util/Util.h
@@ -1,3 +1,4 @@
 #include <Arduino.h>
 
 enum BWSensor {SL,SM,SR}  // Enumeration for BlackWhiteSensors SL = SenorLeft; SM = SensorMiddle; SR = SensorRight
+enum Accuracy {VeryLow, Low,Medium, High, VeryHigh}
