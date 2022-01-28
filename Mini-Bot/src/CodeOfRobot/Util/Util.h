f41eec9d635106d778a6ef852dd129648ac2f8b1
OfCores <40722004+OfCores@users.noreply.github.com>
2022-01-28 14:50:10 +0100

    Merge

--- Mini-Bot/src/CodeOfRobot/Util/Util.h
+++ Mini-Bot/src/CodeOfRobot/Util/Util.h
@@ -1,2 +1,16 @@
 #include <Arduino.h>
 
+//PinDefines
+
+#define PIN_SL 32
+#define PIN_SM 33
+#define PIN_SR 34
+
+#define PIN_LEFT_MOTOR_F 1
+#define PIN_LEFT_MOTOR_B 2
+
+#define PIN_RIGHT_MOTOR_F 1
+#define PIN_RIGHT_MOTOR_B 2
+
+enum BWSensor {SL= PIN_SL,SM = PIN_SM,SR = PIN_SR};  // Enumeration for BlackWhiteSensors SL = SenorLeft; SM = SensorMiddle; SR = SensorRight
+enum Accuracy : int {VeryLow = 10, Low = 50, Medium = 100, High = 500, VeryHigh = 10000};
