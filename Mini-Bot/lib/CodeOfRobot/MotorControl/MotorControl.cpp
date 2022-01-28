861c2c58e5c3e3b6dafa4fe5f06f50e906b8967a
Jones Braun <47891728+JonasBraun04@users.noreply.github.com>
2022-01-28 14:32:11 +0100

    updateSensor

--- Mini-Bot/lib/CodeOfRobot/MotorControl/MotorControl.cpp
+++ Mini-Bot/lib/CodeOfRobot/MotorControl/MotorControl.cpp
@@ -1,12 +1,6 @@
 #include "MotorControl.h"
 
 
-#define PIN_LEFT_MOTOR_F 1
-#define PIN_LEFT_MOTOR_B 2
-
-#define PIN_RIGHT_MOTOR_F 1
-#define PIN_RIGHT_MOTOR_B 2
-
 void MotorControl::setup() {
     
 }
